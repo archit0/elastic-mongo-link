489622359aace541288e8792703fef5d4b1d465a
Archit Dwivedi <architdwivedi00@gmail.com>
2018-03-10 21:35:08 +0530

    Mongodb Now starting as replica set

---
+++ scripts/mongo_entry_point.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+MONGODB=`ping -c 1 mongodb | head -1  | cut -d "(" -f 2 | cut -d ")" -f 1`
+
+echo "Waiting for startup.."
+
+
+
+until mongo --host ${MONGODB}:27017 --eval "print(\"waited for connection\")"
+  do
+    echo "waiting"
+  done
+
+echo "Started.."
+
+
+echo SETUP.sh time now: `date +"%T" `
+mongo --host ${MONGODB}:27017 <<EOF
+   var cfg = {
+        "_id": "rs",
+        "version": 1,
+        "members": [
+            {
+                "_id": 0,
+                "host": "${MONGODB}:27017",
+                "priority": 2
+            }
+        ]
+    };
+    rs.initiate(cfg, { force: true });
+    rs.reconfig(cfg, { force: true });
+EOF
